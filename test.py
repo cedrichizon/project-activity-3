e8354a535397c75b9b5000521b19597b9f0783f5
cedrichizon <113354630+cedrichizon@users.noreply.github.com>
2022-09-23 04:33:10 +0000

    Add files via upload

---
+++ test.py
@@ -0,0 +1,22 @@
+import requests
+from requests import get
+
+ip = get('https://api64.ipify.org').text
+
+response = requests.get("https://geo.ipify.org/api/v2/country,city?apiKey=at_aKdQtFtfKSTzTC44OmrcH6pvEChz8&ipAddress=" + ip).json()
+print("\n=============================================================")
+print("Your Current Public IP Address: " + (response['ip']))
+print("--------------------------LOCATION---------------------------")
+print("Country: " + (response['location']['country']))
+print("Region: " + (response['location']['region']))
+print("City: " + (response['location']['city']))
+print("Latitude: " + str(response['location']['lat']))
+print("Longitude: " + str(response['location']['lng']))
+print("-------------------------------------------------------------")
+print("Internet Service Provider: " + (response['isp']))
+print("Name: " + (response['as']['name']))
+print("Autonomous System Number: " + str(response['as']['asn']))
+print("Route: " + str(response['as']['route']))
+print("Domain: " + str(response['as']['domain']))
+print("Type: " + str(response['as']['type']))
+print("=============================================================")
