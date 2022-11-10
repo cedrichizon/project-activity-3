62bc3e7c4d754925e82f345705c9d403fb975f21
Rein <reinamiel.miranda.cics@ust.edu.ph>
2022-11-10 06:05:02 +0000

    Option B

--- test.py
+++ test.py
@@ -23,4 +23,24 @@ if user == 'A':
     print("Type: " + str(response['as']['type']))
     print("=============================================================")
 
+if user == 'B':
+    ip = input("Enter an IP Address ")
+    response = requests.get("https://geo.ipify.org/api/v2/country,city?apiKey=at_aKdQtFtfKSTzTC44OmrcH6pvEChz8&ipAddress=" + ip).json()
+    #response variable will get the details being asked below from the api
+    print("\n=============================================================")
+    print("Your Current Public IP Address: " + (response['ip'])) #displays the current public ip address
+    print("--------------------------LOCATION---------------------------") #displays the location details
+    print("Country: " + (response['location']['country']))
+    print("Region: " + (response['location']['region']))
+    print("City: " + (response['location']['city']))
+    print("Latitude: " + str(response['location']['lat']))
+    print("Longitude: " + str(response['location']['lng']))
+    print("-------------------------------------------------------------")#displays the isp details
+    print("Internet Service Provider: " + (response['isp']))
+    print("Name: " + (response['as']['name']))
+    print("Autonomous System Number: " + str(response['as']['asn']))
+    print("Route: " + str(response['as']['route']))
+    print("Domain: " + str(response['as']['domain']))
+    print("Type: " + str(response['as']['type']))
+    print("=============================================================")
 
